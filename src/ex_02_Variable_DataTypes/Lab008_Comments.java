2bf15db129a875deaffbfeae8e98705893fa74ea
Pooja <malpurepooja3@gmail.com>
2025-12-20 14:46:47 +0530

    new message

---
+++ src/ex_02_Variable_DataTypes/Lab008_Comments.java
@@ -0,0 +1,15 @@
+package ex_02_Variable_DataTypes;
+
+public class Lab008_Comments {
+    /**
+     * Hello
+     * This is a documentation comment
+     */
+    public static void main(String[] args) {
+        // Method implementation
+
+        //        System.out.println("1");
+        //        System.out.println("2");
+
+    }
+}
